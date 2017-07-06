38e989023fa1c5b69debaacba11f3cea19a9349b
Felipe Soto Campos <fesoca86@gmail.com>
2017-07-06 10:23:49 -0600

    Edit mytest

--- test/mytest.js
+++ test/mytest.js
@@ -1,16 +1,26 @@
 let request = require('supertest')
 let expect = require('chai').expect;
 
+var assert = require('assert');
+
 describe('test.js', () => {
-    it('GET', (done) => {
-        request('http://localhost:8080')
-            .get('/')
-            .end((err, res) => {
-                if (err) {
-                    throw err;
-                }
-                expect(res.status).equal(200);
-                done();
-            });
+    // it('GET', (done) => {
+    //     request('http://localhost:8080')
+    //         .get('/')
+    //         .end((err, res) => {
+    //             if (err) {
+    //                 throw err;
+    //             }
+    //             expect(res.status).equal(200);
+    //             done();
+    //         });
+    // });
+    describe('#indexOf()', function () {
+        it('should return -1 when the value is not present', function () {
+            assert.equal(-1, [1, 2, 3].indexOf(1));
+        });
+        it('should return 1 when the value is present', function () {
+            assert.equal(1, [1, 2, 3].indexOf(2));
+        });
     });
 });
